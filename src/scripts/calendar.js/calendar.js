39e2d09a0b3f488a7834a27a77ede29c7669c6f2
Celestesystems <systemsceleste@gmail.com>
2025-02-07 13:55:44 +0100

    Specified Game Event, Created Calendar Event (Class that Abstracts Calendar Event Element (UI))

---
+++ src/scripts/calendar.js/calendar.js
@@ -0,0 +1,11 @@
+class Calendar{
+    constructor(){
+
+    }
+}
+
+class CalendarEvent{
+    constructor(game_event){
+        this.game_event = game_event;
+    }
+}
--- src/scripts/game_events/game_event.js
+++ src/scripts/game_events/game_event.js
@@ -1,7 +1,12 @@
 class GameEvent{
-    constructor(title, description){
+    constructor(title, description, date, time_begin, time_end){
         this.title = title;
         this.description = description;
+
+        this.date = date;
+
+        this.timeBegin = time_begin;
+        this.timeEnd = time_end;
     }
 
     GetTitle(){
@@ -10,6 +15,18 @@ class GameEvent{
     
     GetDescription(){
         return this.description;
+    }
+
+    GetDate(){
+        return this.date;
+    }
+
+    getTimeBegin(){
+        return this.timeBegin;
+    }
+
+    getTimeEnd(){
+        return this.timeEnd;
     }
 }
 
