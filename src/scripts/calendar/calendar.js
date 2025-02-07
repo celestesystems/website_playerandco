2d2787d97b3c0a00ae4c291b23ec0021bdf95fc2
Celestesystems <systemsceleste@gmail.com>
2025-02-07 14:27:38 +0100

    Setting up Calendar Display

---
+++ src/scripts/calendar/calendar.js
@@ -0,0 +1,25 @@
+import { GameEvent } from "../game_events/game_event.js";
+
+class Calendar{
+    Calendar_elements;
+
+    constructor(amount){
+
+    }
+
+    Display(){
+
+    }
+}
+
+class CalendarEvent{
+    constructor(game_event){
+        this.game_event = game_event;
+    }
+
+    Display(){
+
+    }
+}
+
+export default {};
